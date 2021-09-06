4f89dbc9030a831b7e6de1f30d5339504d1a77f1
Mouatamid <mouatamid.ela@gmail.com>
2021-09-06 12:10:43 +0100

    Adding pagination

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -9,26 +9,55 @@ class Container extends React.Component {
     this.state = {
       repos: [],
       loaded: false,
+      pageNumber: 1,
     };
+    this.nextButtonClicked = this.nextButtonClicked.bind(this);
+    this.backButtonClicked = this.backButtonClicked.bind(this);
   }
 
   componentDidMount() {
-    console.log(this.getDateFormat());
-    axios
-      .get(
-        "https://api.github.com/search/repositories?q=created:>" +
+    let link =
+      this.state.pageNumber == 1
+        ? "https://api.github.com/search/repositories?q=created:>" +
           this.getDateFormat() +
           "&sort=stars&order=desc"
-      )
+        : "https://api.github.com/search/repositories?q=created:>" +
+          this.getDateFormat() +
+          "&sort=stars&order=desc&page=" + this.state.pageNumber;
+    axios
+      .get(link)
       .then((result) => {
         return result.data;
       })
       .then((data) => {
         this.setState({
-            repos : data.items,
-            loaded : true
-        })
-      }).catch(e => window.alert(e.message));
+          repos: data.items,
+          loaded: true,
+        });
+      })
+      .catch((e) => window.alert(e.response.data.message));
+  }
+  componentDidUpdate() {
+    let link =
+      this.state.pageNumber == 1
+        ? "https://api.github.com/search/repositories?q=created:>" +
+          this.getDateFormat() +
+          "&sort=stars&order=desc"
+        : "https://api.github.com/search/repositories?q=created:>" +
+          this.getDateFormat() +
+          "&sort=stars&order=desc&page=" + this.state.pageNumber;
+    axios
+      .get(link)
+      .then((result) => {
+        return result.data;
+      })
+      .then((data) => {
+        this.setState({
+          repos: data.items,
+          loaded: true,
+        });
+      })
+      .catch((e) => window.alert(e.response.data.message));
   }
   getDateFormat() {
     let date = new Date();
@@ -36,6 +65,18 @@ class Container extends React.Component {
     let month = date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth();
     let day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
     return `${fullYear}-${month}-${day}`;
+  }
+  nextButtonClicked() {
+    this.setState({
+      loaded: false,
+      pageNumber: this.state.pageNumber + 1,
+    });
+  }
+  backButtonClicked(){
+    this.setState({
+      loaded: false,
+      pageNumber: this.state.pageNumber - 1,
+    });
   }
   render() {
     return (
@@ -45,6 +86,18 @@ class Container extends React.Component {
         ) : (
           <h3 className="loading">Loading...</h3>
         )}
+        <div className="buttons">
+          {this.state.pageNumber == 1 ? (
+            <button className="backButton"  disabled>
+              Back
+            </button>
+          ) : (
+            <button className="backButton" onClick={this.backButtonClicked}>Back</button>
+          )}
+          <button className="nextButton" onClick={this.nextButtonClicked}>
+            Next
+          </button>
+        </div>
       </div>
     );
   }
